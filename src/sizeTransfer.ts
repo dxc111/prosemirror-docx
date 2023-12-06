607e07cc665d0ebccd8c06ef54abe6b426b84590
dxc111 <dx131477@outlook.com>
2023-12-06 17:50:10 +0800

    feat：字号等大小计算

--- src/sizeTransfer.ts
+++ src/sizeTransfer.ts
@@ -1,7 +1,8 @@
 /**
- * 尺寸转换 像素 14 == 20
+ * 尺寸转换 像素 14 == 14, xml * 2
  * @param px
  */
 export default function sizeTransfer(px: number) {
-  return (px * 20) / 14;
+  // return (px * 20) / 14;
+  return px * 2;
 }
